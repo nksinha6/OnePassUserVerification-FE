62c2a518ffee958fc0036b3fb863d31ec6546774
Nishant <sinha.nishant@outlook.com>
2026-01-29 00:33:48 +0530

    import checkin status

--- src/router.jsx
+++ src/router.jsx
@@ -14,6 +14,7 @@ import ProtectedRoute from "@/components/ProtectedRoute";
 import SuccessPage from "@/pages/SuccessPage";
 import UserDetails from "./pages/UserDetails";
 import VerificationCallbackPage from "./pages/VerificationCallbackPage";
+import CheckinStatusPage from "./pages/CheckInStatusPage";
 
 import { ROUTES } from "@/constants/ui";
 
